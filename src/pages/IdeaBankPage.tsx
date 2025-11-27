fcab85bd210c113d9b7daa47427092fb2a27058b
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:06:22 +0100

    Create IdeaBankPage.tsx

---
+++ src/pages/IdeaBankPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const IdeaBankPage: React.FC = () => {
+  return (
+    <main className="page page-idebank">
+      <h1>Idebank</h1>
+      <p>
+        Her kan du foreslå funksjoner og nye verktøy. Firestore-skjema bygges i
+        Blokk 6.
+      </p>
+    </main>
+  );
+};
+
+export default IdeaBankPage;
