324913298a501c1e08ca0d9bf46f33f3525e2b00
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 14:28:44 +0100

    Update StatsPage.tsx

--- src/admin/StatsPage.tsx
+++ src/admin/StatsPage.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const StatsPage: React.FC = () => {
   return (
-    <main className="page">
+    <main className="page admin-subpage">
       <h1>Statistikk</h1>
-      <p>Placeholder.</p>
+      <p>Analyse, grafikk og innsikt kommer etter lansering.</p>
     </main>
   );
 };
