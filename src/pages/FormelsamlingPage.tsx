8911524971e694add7c5436eb3702608e729c72f
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:06:07 +0100

    Create FormelsamlingPage.tsx

---
+++ src/pages/FormelsamlingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FormelsamlingPage: React.FC = () => {
+  return (
+    <main className="page page-formelsamling">
+      <h1>Digital Formelsamling</h1>
+      <p>
+        Denne siden blir produktpresentasjonen for Formelsamlingen. Stripe
+        Checkout integreres i Blokk 5.
+      </p>
+    </main>
+  );
+};
+
+export default FormelsamlingPage;
