b479de38ed31d294aa62a223ee7c88a877015acb
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:09:13 +0100

    Create StatsPage.tsx

---
+++ src/admin/StatsPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StatsPage: React.FC = () => {
+  return (
+    <main className="page">
+      <h1>Statistikk</h1>
+      <p>Placeholder.</p>
+    </main>
+  );
+};
+
+export default StatsPage;
