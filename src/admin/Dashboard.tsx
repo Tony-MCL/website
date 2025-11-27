093b0b9fd09863ca381815aa304eed541edd2974
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:08:09 +0100

    Create Dashboard.tsx

---
+++ src/admin/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <main className="page">
+      <h1>Dashboard</h1>
+      <p>Her kommer nøkkeltall i senere blokker.</p>
+    </main>
+  );
+};
+
+export default Dashboard;
