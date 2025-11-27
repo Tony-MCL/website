6dc5b4784f0babfa31ffb87e47819e686249b579
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:18:12 +0100

    Create WatermarkLayer.tsx

---
+++ src/components/WatermarkLayer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WatermarkLayer: React.FC = () => {
+  return (
+    <div className="watermark-layer">
+      <img src="/mcl-watermark.svg" alt="MCL watermark" />
+    </div>
+  );
+};
+
+export default WatermarkLayer;
