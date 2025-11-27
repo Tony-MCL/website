555b2136fd69e683ec95175722d6cb3f7f0d1c9f
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:08:25 +0100

    Create CustomersPage.tsx

---
+++ src/admin/CustomersPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CustomersPage: React.FC = () => {
+  return (
+    <main className="page">
+      <h1>Kunder</h1>
+      <p>Placeholder.</p>
+    </main>
+  );
+};
+
+export default CustomersPage;
