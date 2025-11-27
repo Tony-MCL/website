0a6f0335ad9073acb2129059a3476f9fa18e3297
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:06:52 +0100

    Create ContactPage.tsx

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ContactPage: React.FC = () => {
+  return (
+    <main className="page page-contact">
+      <h1>Kontakt oss</h1>
+      <p>
+        Her kommer et kontaktskjema i neste blokk som lagrer i Firestore.
+      </p>
+    </main>
+  );
+};
+
+export default ContactPage;
