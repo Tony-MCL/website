b5735436dff00f455e70e9b07f7a1cf01e6fb0a9
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:08:57 +0100

    Create MessagesPage.tsx

---
+++ src/admin/MessagesPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MessagesPage: React.FC = () => {
+  return (
+    <main className="page">
+      <h1>Meldinger</h1>
+      <p>Placeholder.</p>
+    </main>
+  );
+};
+
+export default MessagesPage;
