e9739c9e7dc5c3efe694c3660bbe9b4f00df5921
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 12:08:43 +0100

    Create LicensesPage.tsx

---
+++ src/admin/LicensesPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LicensesPage: React.FC = () => {
+  return (
+    <main className="page">
+      <h1>Lisenser</h1>
+      <p>Placeholder.</p>
+    </main>
+  );
+};
+
+export default LicensesPage;
