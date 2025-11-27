ee61c6c715358593c3bccbaf3107bf83621c0965
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-11-27 14:27:42 +0100

    Update Dashboard.tsx

--- src/admin/Dashboard.tsx
+++ src/admin/Dashboard.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Dashboard: React.FC = () => {
   return (
-    <main className="page">
-      <h1>Dashboard</h1>
-      <p>Her kommer nøkkeltall i senere blokker.</p>
+    <main className="page admin-subpage">
+      <h1>Admin Dashboard</h1>
+      <p>Her kommer mer detaljert oversikt i neste versjoner.</p>
     </main>
   );
 };
