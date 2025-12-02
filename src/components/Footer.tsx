fef75c9a937e2fc0d7d2e9e5a81c44060b2dd48f
Tony-MCL <MorningCoffeeLabs@gmail.com>
2025-12-02 13:10:13 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,45 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <footer className="mcl-footer">
+    <footer className="footer">
       <div className="footer-inner">
-
-        {/* Venstre seksjon */}
-        <div className="footer-col">
-          <h4>Mathisens Morning Coffee Labs</h4>
-          <p className="footer-small">© {new Date().getFullYear()} Alle rettigheter forbeholdt.</p>
-        </div>
-
-        {/* Midtseksjon: Juridiske lenker */}
-        <div className="footer-col">
-          <h4>Juridisk</h4>
-          <ul className="footer-links">
-            <li>
-              <Link to="/kjopsvilkar">Kjøpsvilkår</Link>
-            </li>
-            <li>
-              <Link to="/personvern">Personvern</Link>
-            </li>
-            <li>
-              <Link to="/brukervilkar">Brukervilkår</Link>
-            </li>
-            <li>
-              <Link to="/refusjon">Refusjon</Link>
-            </li>
-          </ul>
-        </div>
-
-        {/* Høyre seksjon: Kontakt */}
-        <div className="footer-col">
-          <h4>Kontakt</h4>
-          <p className="footer-small">
-            support@morningcoffeelabs.no
-            <br />
-            Norge
-          </p>
-        </div>
-
+        <span className="footer-copy">
+          Morning Coffee Labs © {year}
+        </span>
+        <nav className="footer-links">
+          <Link to="/kjopsvilkar">Kjøpsvilkår</Link>
+          <span>·</span>
+          <Link to="/brukervilkar">Brukervilkår</Link>
+          <span>·</span>
+          <Link to="/personvern">Personvern</Link>
+        </nav>
       </div>
     </footer>
   );
